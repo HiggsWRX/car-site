406a7a247d0fdd4f707a92acae9a6daa1bf9de34
Hiago Oliveira <hiagooliveira@thesting.com>
2022-07-05 22:20:21 +0200

    Add hamburguer menu and sidebar initial

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Hamburguer from "./Hamburguer.svelte"
+    import Logo from "./Logo.svelte"
+
+    export let showSidebar = false
+</script>
+
+<header class="flex items-center h-10 bg-gray-300">
+    <nav class="flex w-full justify-between">
+        <Logo />
+        <Hamburguer bind:open={showSidebar} />
+    </nav>
+</header>
---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let isOpen = false
+</script>
+
+<aside class="absolute w-full h-full bg-gray-200 border-r-2 shadow-lg" class:isOpen>
+	<nav class="p-12 text-xl">
+		<a class="block" href="#about">About</a>
+		<a class="block" href="#contact">Contact</a>
+	</nav>
+</aside>
+
+<style>
+	aside {
+		right: -100%;
+		transition: right 0.3s ease-in-out
+	}
+	
+	.isOpen {
+		right: 0
+	}
+</style>
