4d768d9c94380a6ba2a89502bc71a0aa8d721b85
Hiago Oliveira <hiagooliveira@thesting.com>
2022-07-12 16:00:02 +0200

    Go to root when click logo

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
+
   import Hamburguer from './Hamburguer.svelte'
   import Logo from './Logo.svelte'
   import Menu from './Menu.svelte'
@@ -7,7 +9,9 @@
 </script>
 
 <nav class="flex items-center h-16 w-full pl-3 md:p-8 justify-between">
-  <Logo />
+  <div on:click={() => goto('/')}>
+    <Logo />
+  </div>
   <ul class="hidden md:flex gap-4 text-lg [&>*:hover]:underline">
     <Menu />
   </ul>
