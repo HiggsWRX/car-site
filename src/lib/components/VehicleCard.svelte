0f2f0d5c19b7ceec5c1c2cad40cbc0c187bc1581
Hiago Oliveira <hiagooliveira@thesting.com>
2022-07-14 14:23:16 +0200

    Fix: price always on lowest point

--- src/lib/components/VehicleCard.svelte
+++ src/lib/components/VehicleCard.svelte
@@ -6,7 +6,7 @@
   export let vehicle: Vehicle
 </script>
 
-<div class="md:w-1/4 text-slate-600">
+<div class="flex flex-col md:w-1/4 text-slate-600">
   <div class="h-[30vh] rounded-sm overflow-hidden">
     <img alt="car" src={vehicle.images[0]} class="w-full h-full object-cover" />
   </div>
@@ -18,5 +18,7 @@
     <h4>Brandstof: {vehicle.fuel}</h4>
     <h4>Transmissie: {vehicle.transmission}</h4>
   </div>
-  <h3 class="p-2 text-right text-2xl md:text-3xl">{formatCurrency(vehicle.price)}</h3>
+  <h3 class="p-2 mt-auto text-right text-2xl md:text-3xl">
+    {formatCurrency(vehicle.price)}
+  </h3>
 </div>
